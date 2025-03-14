dcab17f69a8fd7fe8f4b10928862508f6fc207ed
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2025-03-14 14:24:09 +0800

    add moonlight package
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

---
+++ package/moonlight/moonlight-embedded.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# moonlight-embedded
+#
+################################################################################
+
+MOONLIGHT_VERSION = v2.7.0
+MOONLIGHT_SITE = https://github.com/moonlight-stream/moonlight-embedded.git
+MOONLIGHT_SITE_METHOD = git
+MOONLIGHT_INSTALL_STAGING = YES
+MOONLIGHT_GIT_SUBMODULES = YES
+
+$(eval $(cmake-package))
