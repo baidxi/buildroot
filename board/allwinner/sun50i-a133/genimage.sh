f451479eb8c32755a3c8fcabbc5dbcc2d0a80cbc
jeck.chen <jeck.chen@dbappsecurity.com.cn>
2025-04-17 23:10:36 +0800

    fix rootfs mount
    Signed-off-by: jeck.chen <jeck.chen@dbappsecurity.com.cn>

---
+++ board/allwinner/sun50i-a133/genimage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+support/scripts/genimage.sh -c board/allwinner/sun50i-a133/genimage-sdcard.cfg
+support/scripts/genimage.sh -c board/allwinner/sun50i-a133/genimage-emmc.cfg
--- board/allwinner/sun50i-a133/post-build.sh
+++ board/allwinner/sun50i-a133/post-build.sh
@@ -5,5 +5,6 @@
 
 BOARD_DIR="$(dirname "$0")"
 
-install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${BINARIES_DIR}/extlinux.conf"
+install -m 0644 -D "${BOARD_DIR}/extlinux-sdcard.conf" "${BINARIES_DIR}/extlinux-sdcard.conf"
+install -m 0644 -D "${BOARD_DIR}/extlinux-emmc.conf" "${BINARIES_DIR}/extlinux-emmc.conf"
 install -m 0644 -D "${BOARD_DIR}/uboot.env" "${BINARIES_DIR}/uboot.env"
